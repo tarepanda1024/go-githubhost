e3942da607b6f9b4dcf0dd23bb17dfe8140e8d0b
liuxiaodong <liuxiaodong2017@gmail.com>
2021-08-23 19:10:49 +0800

    change: 直接在命令行输出，不再提供web服务

--- main.go
+++ main.go
@@ -2,31 +2,13 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
-
 	"go-githubhost/runner"
 )
 
 func main() {
-	_ = Run()
-}
-
-func Run() (err error) {
-	// http server
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		text, err := runner.Run()
-		if err != nil {
-			_, _ = fmt.Fprintf(writer, fmt.Sprintf("异常: %v", err))
-		}
-		_, _ = fmt.Fprintln(writer, text)
-	})
-
-	err = http.ListenAndServe(":9090", nil)
+	text, err := runner.Run()
 	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-		return
+		panic(err)
 	}
-
-	return
+	fmt.Println(text)
 }
